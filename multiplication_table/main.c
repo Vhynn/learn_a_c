b6c9d2989f32af2137392a359a4705d7696abef7
Jeffrey Brown <jbradenbrown@gmail.com>
2022-06-15 20:43:55 -0500

    made some folders

---
+++ multiplication_table/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Create a program that will print out a multiplication
+// table for numbers 1-X.
+
+int main() {
+
+	
+	return 0;
+}
+
+int** mtable(int lower, int upper) {
+
+}
+
+
